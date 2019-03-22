10421e159d505be3213690aaa0e8f1104621585c
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2019-03-22 15:25:44 +0100

    Level 1

--- src/cli.py
+++ src/cli.py
@@ -4,20 +4,33 @@ import json
 from main import main
 from pprint import pprint
 
+
+def parse(lines):
+    x = int(lines[0].split()[0])
+    y = int(lines[0].split()[1])
+    cmds = []
+
+    it = iter(lines[1].split())
+    for e in it:
+        cmds.append((e, int(next(it))))
+
+    return {"x": x, "y": y, "cmds": cmds}
+
+
 if __name__ == "__main__":
-    level, quests = 0, 4
-    for i in range(quests):
-        input_file = r'..\data\level{0}\level{0}_{1}.json'.format(level, i)
+    level, quests = 1, 6
+    for i in range(1, quests):
+        input_file = r'..\data\level{0}\level{0}_{1}.in'.format(level, i)
         output_file = os.path.splitext(input_file)[0] + ".out"
 
         with open(input_file, 'r') as fi:
-            input = json.load(fi)
-            pprint(input)
+            data = parse(fi.readlines())
+            # pprint(data)
 
             print("=== Input {}".format(i))
             print("======================")
 
-            result = main(input)
+            result = main(data)
             pprint(result)
 
             with open(output_file, 'w+') as fo:
--- src/main.py
+++ src/main.py
@@ -1,2 +1,32 @@
-def main(input):
-    return "Result"
+import enum
+
+
+class Direction(enum.Enum):
+    Right = 0
+    Down = 1
+    Left = 2
+    Up = 3
+
+    def turn(self, ntimes=1):
+        return Direction((self.value + ntimes) % 4)
+
+    def move(self, x, y, ntimes=1):
+        if self == Direction.Right:
+            return x + ntimes, y
+        elif self == Direction.Down:
+            return x, y + ntimes
+        elif self == Direction.Left:
+            return x - ntimes, y
+        elif self == Direction.Up:
+            return x, y - ntimes
+
+
+def main(data):
+    x, y, d = data["x"], data["y"], Direction.Right
+    for command, ntimes in data["cmds"]:
+        if command == "F":
+            x, y = d.move(x, y, ntimes)
+        elif command == "T":
+            d = d.turn(ntimes)
+
+    return "{} {}".format(x, y)
