508bf271c0902fbd40107e048a71cf78ffbc7287
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2019-03-22 14:43:10 +0100

    Initial commit with template

---
+++ README.md
@@ -0,0 +1,19 @@
+# 30th Catalysts Coding Contest
+
+My solutions for the [30th Catalysts Coding Contest](https://codingcontest.org/) in March 2019, written in Python.
+
+:goat: :goat: :goat:
+
+## Challenges
+
+- :x: **[Level 1](data/level1/Level1.pdf)** - *unfinished*
+
+## Requirements
+
+### Python >= 3.6
+
+Package requirements are specified in the [requirements.txt](requirements.txt) file.
+
+```
+pip3 install -r requirements.txt
+```
---
+++ src/cli.py
@@ -0,0 +1,24 @@
+import os
+import json
+
+from main import main
+from pprint import pprint
+
+if __name__ == "__main__":
+    level, quests = 0, 4
+    for i in range(quests):
+        input_file = r'..\data\level{0}\level{0}_{1}.json'.format(level, i)
+        output_file = os.path.splitext(input_file)[0] + ".out"
+
+        with open(input_file, 'r') as fi:
+            input = json.load(fi)
+            pprint(input)
+
+            print("=== Input {}".format(i))
+            print("======================")
+
+            result = main(input)
+            pprint(result)
+
+            with open(output_file, 'w+') as fo:
+                fo.write(result)
