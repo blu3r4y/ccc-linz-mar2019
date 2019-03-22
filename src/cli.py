bba6ac4095df58e39dce131c8b92e5db98fd0f69
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2019-03-22 19:01:03 +0100

    Level 4 - unfinished

--- src/cli.py
+++ src/cli.py
@@ -19,8 +19,9 @@ def parse(lines):
     for e in it:
         cmds.append((e, int(next(it))))
 
-    # speed
-    speed = float(lines[3])
+    # health and speed
+    health = float(lines[3].split()[0])
+    speed = float(lines[3].split()[1])
 
     # spawn times
     nspawn = int(lines[4])
@@ -28,28 +29,35 @@ def parse(lines):
     for i in range(nspawn):
         spawns.append(int(lines[4 + i + 1]))
 
+    # damage and range
+    damage = float(lines[4 + nspawn + 1].split()[0])
+    towerrange = int(lines[4 + nspawn + 1].split()[1])
+
     # queries
-    nqueries = int(lines[4 + nspawn + 1])
-    queries = []
-    for i in range(nqueries):
-        qtxt = lines[4 + nspawn + 2 + i]
-        qtime = int(qtxt.split()[0])
-        qid = int(qtxt.split()[1])
-        queries.append((qtime, qid))
+    t = int(lines[4 + nspawn + 2])
+    towers = []
+    for i in range(t):
+        towertxt = lines[4 + nspawn + 3 + i]
+        towerx = int(towertxt.split()[0])
+        towery = int(towertxt.split()[1])
+        towers.append((towerx, towery))
 
     return {
         "wx": wx, "wy": wy,
         "x": x, "y": y,
         "cmds": cmds,
         "speed": speed,
+        "health": health,
+        "damage": damage,
+        "range": towerrange,
         "spawns": spawns,
-        "queries": queries
+        "towers": towers
     }
 
 
 if __name__ == "__main__":
-    level, quests = 3, 5
-    for i in range(0, quests + 1):
+    level, quests = 4, 1
+    for i in range(1, quests + 1):
         input_file = r'..\data\level{0}\level{0}_{1}.in'.format(level, i)
         output_file = os.path.splitext(input_file)[0] + ".out"
 
--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import enum
 from collections import defaultdict
 from abc import abstractmethod
 from pprint import pprint
+import itertools
+import numpy as np
 
 from colorama import Fore, Back, Style
 
@@ -36,18 +38,82 @@ class Cell:
         self.y = y
 
 
+class TowerState(enum.Enum):
+    Seeking = 0
+    Locked = 1
+
+    def switch(self):
+        return TowerState.Seeking if self == TowerState.Locked else TowerState.Locked
+
+
+class Tower(Cell):
+
+    def __init__(self, maze, x, y, damage, towerrange):
+        super().__init__(maze, x, y)
+        self.damage = damage
+        self.towerrange = towerrange
+        self.state = TowerState.Seeking
+        self.target = None
+
+    def lock_on_new_alien(self):
+        distances = [(a, self.position_to_alien(a))
+                     for a in self.maze.aliens.values()
+                     if a.alive()]
+
+        # filter out of range targets away
+        distances = [e for e in distances if e[1] <= self.towerrange]
+
+        if len(distances) > 0:
+            ordered = sorted(distances, key=lambda e: (e[1], e[0].identifier))
+            self.target = ordered[0][0]
+            self.state = TowerState.Locked
+            print("Tower #{}: Locked on target in range {}".format(hash(self), ordered[0][1]))
+        else:
+            # nothing to lock
+            self.target = None
+            self.state = TowerState.Seeking
+            print("Tower #{}: No target found".format(hash(self)))
+
+    def calculate_shot(self):
+        if self.target:
+            # target dead or out of range?
+            target_out_of_range = self.position_to_alien(self.target) > self.towerrange
+            if not self.target.alive() or target_out_of_range:
+                self.state = TowerState.Seeking
+                print("Tower #{}: Target dead or out of range, removed.".format(hash(self)))
+
+        if self.state == TowerState.Seeking:
+            self.lock_on_new_alien()
+
+        if self.state == TowerState.Locked:
+            return self.target
+
+        return None
+
+    def shot_for_real(self, target):
+        target.get_shot_by(self)
+
+    def position_to_alien(self, alien):
+        a = np.array([self.x, self.y])
+        # b = np.array([alien.x, alien.y])
+        b = np.array([alien.xexact, alien.yexact])
+        norm = np.linalg.norm(a - b)
+        return norm
+
+
 class Alien(Cell):
 
-    def __init__(self, maze, x, y, identifier, speed, sequence, direction=Direction.Right):
+    def __init__(self, maze, x, y, identifier, health, speed, sequence, direction=Direction.Right):
         super().__init__(maze, x, y)
         self.identifier = identifier
         self.direction = direction
         self.sequence = sequence
+        self.health = health
         self.speed = speed
 
         # internal offset
-        self.dx = 0.0
-        self.dy = 0.0
+        # self.dx = 0.0
+        # self.dy = 0.0
 
         # sequence position and internal sequence position
         self.si = 0
@@ -55,12 +121,22 @@ class Alien(Cell):
         self.seq_old = 0.0
 
     @property
-    def xfloat(self):
-        return self.x + self.dx
+    def xexact(self):
+        return self.exact_position()[0]
 
     @property
-    def yfloat(self):
-        return self.y + self.dy
+    def yexact(self):
+        return self.exact_position()[1]
+
+    def alive(self):
+        return self.health > 0
+
+    def get_shot_by(self, tower):
+        self.health -= tower.damage
+        print("Alien {} got damage {}. New health {}".format(self.identifier, tower.damage, self.health))
+
+    def exact_position(self):
+        return self.direction.move(self.x, self.y, speed=self.seq_old)
 
     def step(self):
         seq_steps, seq_delta = divmod(self.speed + self.seq_old, 1.0)
@@ -94,13 +170,11 @@ class Alien(Cell):
                 # forward move costs tick
                 i += 1
 
+    def reached_base(self):
+        return self.si == len(self.sequence)
+
     def move(self):
-        xold, yold = self.xfloat, self.yfloat
-        x, y = self.direction.move(self.xfloat, self.yfloat, speed=1.0)
-        xnew = math.floor(x)
-        ynew = math.floor(y)
-        self.dx = x % 1.0
-        self.dy = y % 1.0
+        xnew, ynew = self.direction.move(self.x, self.y, speed=1)
 
         # let's move there ...
         if self.x != xnew or self.y != ynew:
@@ -119,6 +193,7 @@ class Maze(object):
         self.width = width
         self.height = height
         self.aliens = {}
+        self.towers = []
         self.grid = [[[] for _ in range(width)] for _ in range(height)]
         self.tick = 0
 
@@ -126,11 +201,49 @@ class Maze(object):
         self.aliens[alien.identifier] = alien
         self.grid[alien.x][alien.y].append(alien)
 
-    def step(self):
+    def add_tower(self, tower: Tower):
+        self.towers.append(tower)
+        self.grid[tower.x][tower.y].append(tower)
+
+    def update_aliens(self):
         for alien in self.aliens.values():
             alien.step()
 
         self.tick += 1
+
+    def update_towers(self):
+        targets = []
+        for tower in self.towers:
+            target = tower.calculate_shot()
+            if target:
+                targets.append((target, tower))
+
+        # finally ..
+        for target, tower in targets:
+            tower.shot_for_real(target)
+
+    def any_alien_reached_base(self):
+        # check if any alien reached the base
+        reached_base = False
+
+        for alien in self.aliens.values():
+            # any alien reached the base?
+            if alien.reached_base():
+                reached_base = True
+                break
+
+        return reached_base
+
+    def all_aliens_dead(self):
+        # check if all aliens are dead
+        num_alive = 0
+
+        for alien in self.aliens.values():
+            # alien still alive?
+            if alien.alive():
+                num_alive += 1
+
+        return num_alive == 0
 
     def move_to_new_position(self, a: Cell, xnew, ynew):
         self.grid[a.x][a.y].remove(a)
@@ -167,40 +280,60 @@ def main(data):
     # initial position and speed
     x, y = data["x"], data["y"]
     speed = data["speed"]
+    health = data["health"]
 
     sequence = data["cmds"]
 
+    damage = data["damage"]
+    towerrange = data["range"]
+
     # build aliens
+    num_aliens = len(data["spawns"])
     aliens = defaultdict(list)
     for i, spawn_tick in enumerate(data["spawns"]):
-        alien = Alien(maze, x, y, identifier=i, speed=speed, sequence=sequence)
+        alien = Alien(maze, x, y, identifier=i, health=health, speed=speed, sequence=sequence)
         aliens[spawn_tick].append(alien)
 
-    # build query lookup
-    queries = defaultdict(list)
-    for query_pos, (tick, alien_id) in enumerate(data["queries"]):
-        queries[tick].append((query_pos, alien_id))
+    # build and towers
+    for towerx, towery in data["towers"]:
+        tower = Tower(maze, towerx, towery, damage, towerrange)
+        maze.add_tower(tower)
 
-    answers = [None] * len(data["queries"])
-    max_tick = max(queries.keys())
+    result = "TIE"
+    tick = 0
 
     # simulate game
-    for tick in range(max_tick + 1):
-        # print(maze)
+    while True:
 
-        maze.step()
+        # 1) update all alien positions
+        maze.update_aliens()
 
-        # spawn aliens
+        # 2) check if any alien has reached the end?
+        reached_base = maze.any_alien_reached_base()
+        if reached_base:
+            result = "LOSS"
+            print("Alien reached the base at tick {}".format(tick))
+            break
+
+        # 3) spawn new aliens
         if tick in aliens.keys():
             for alien in aliens[tick]:
                 maze.add_alien(alien)
 
-        # answer queries
-        if tick in queries.keys():
-            for query_pos, alien_id in queries[tick]:
-                alien = maze.aliens[alien_id]
-                answers[query_pos] = (tick, alien_id, alien.x, alien.y)
+        # 4) simulate tower shots
+        if tick > 0:
+            maze.update_towers()
 
-    # print(answers)
+        # 5) check for dead aliens
+        all_dead = maze.all_aliens_dead()
 
-    return "\n".join(["{} {} {} {}".format(*vals) for vals in answers])
+        # 6) check if all aliens are dead and no more will be spawning
+        no_more_spawning = len(maze.aliens) == num_aliens
+        if no_more_spawning and all_dead:
+            result = "WIN"
+            print("All aliens are dead at tick {}".format(tick))
+            break
+
+        tick += 1
+
+    return "\n".join([str(tick), result])
